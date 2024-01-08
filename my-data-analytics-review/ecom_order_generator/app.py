3021369cb1b0e284ea52e9b45783e5de0e52b791
bharatkatyal <bharatkatyal@live.com>
2024-01-08 16:42:58 -0500

    first commit

---
+++ my-data-analytics-review/ecom_order_generator/app.py
@@ -0,0 +1,45 @@
+import boto3
+from datetime import datetime
+import random
+import time
+import json
+import calendar  # Add this line to import the calendar module
+import os
+
+
+
+
+# The kinesis stream defined in AWS console
+stream_name = os.environ['KinesisStreamName']
+
+k_client = boto3.client('kinesis', region_name='us-east-1')
+
+def lambda_handler(event, context):
+    for i in range(10):
+        order_id = random.randint(1000, 9999)
+        customer_id = random.randint(100, 999)
+        product_id = random.randint(1, 100)
+        quantity = random.randint(1, 10)
+        price = round(random.uniform(10, 500), 2)
+        order_timestamp = calendar.timegm(datetime.utcnow().timetuple())
+
+        order_data = {
+            'order_id': order_id,
+            'customer_id': customer_id,
+            'product_id': product_id,
+            'quantity': quantity,
+            'price': price,
+            'order_timestamp': order_timestamp
+        }
+
+        put_to_stream(order_data, order_id, order_timestamp)
+        time.sleep(1)
+
+def put_to_stream(order_data, order_id, order_timestamp):
+    payload = json.dumps(order_data)
+    print(payload)
+
+    put_response = k_client.put_record(
+                    StreamName=stream_name,
+                    Data=payload,
+                    PartitionKey=str(order_id))
