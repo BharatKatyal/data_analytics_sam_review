53e0bab6d9b12b3b416fe106054dc2505465aaf1
bharatkatyal <bharatkatyal@live.com>
2024-01-08 17:23:18 -0500

    saving progress, sam deployed issues with delivery in s3

--- my-data-analytics-review/ecom_order_generator/app.py
+++ my-data-analytics-review/ecom_order_generator/app.py
@@ -15,7 +15,7 @@ stream_name = os.environ['KinesisStreamName']
 k_client = boto3.client('kinesis', region_name='us-east-1')
 
 def lambda_handler(event, context):
-    for i in range(10):
+    for _ in range(10):
         order_id = random.randint(1000, 9999)
         customer_id = random.randint(100, 999)
         product_id = random.randint(1, 100)
@@ -32,14 +32,15 @@ def lambda_handler(event, context):
             'order_timestamp': order_timestamp
         }
 
-        put_to_stream(order_data, order_id, order_timestamp)
+        put_to_stream(order_data, order_id)
+
         time.sleep(1)
 
-def put_to_stream(order_data, order_id, order_timestamp):
+def put_to_stream(order_data, order_id):
     payload = json.dumps(order_data)
     print(payload)
 
-    put_response = k_client.put_record(
-                    StreamName=stream_name,
-                    Data=payload,
-                    PartitionKey=str(order_id))
+    k_client.put_record(
+        StreamName=stream_name,
+        Data=payload,
+        PartitionKey=str(order_id))
